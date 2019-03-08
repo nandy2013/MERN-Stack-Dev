e2d6031af04225ff5b8aa215d22aec699f746059
Nandhirajan P <1217688@TCS.com>
2019-03-09 00:54:05 +0530

    Express setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 //DB Config
@@ -8,12 +12,18 @@ const db = require("./config/keys").mongoURI;
 
 //Connect to  MongoDB
 mongoose
-  //.connect("mongodb://localhost/test", { useNewUrlParser: true })
-  .connect(db, { useNewUrlParser: true })
+  .connect("mongodb://localhost/test", { useNewUrlParser: true })
+  //.connect(db, { useNewUrlParser: true })
   //.connect("localhost/DEV_CONNECT", { useNewUrlParser: true })
   .then(() => console.log("MongoDBConnected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello123"));
+
+//Use routes
+app.use("/api/users", users);
+app.use("/api/profiles", profiles);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server running on port ${port}"));
